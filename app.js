4a6bbeb34ca1fde6cc8caf8104e575885c94f070
Preston Richey <prichey@barkleyus.com>
2017-08-08 14:42:46 -0500

    Add layouts and basic auth

--- app.js
+++ app.js
@@ -3,26 +3,46 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
+const cookieParser = require('cookie-parser');
+const flash = require('express-flash');
 const http = require('http').Server(app);
 const path = require('path');
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); //for parsing url encoded
+const routes = require('./routes');
+
+function checkAuth (req, res, next) {
+  console.log('checkAuth ' + req.url);
+  console.log('checking auth');
+
+  // don't serve /secure to those not logged in
+  // you should add to this list, for each and every secure url
+  if (req.url === '/admin' && (!req.session || !req.session.authenticated)) {
+    res.redirect('/login');
+    return;
+  }
+
+  next();
+}
 
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(require('express-session')({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+
+app.use(checkAuth);
+app.use('/', routes);
+
 require('./botkit-controller')(app);
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-app.get('*', function(req, res) {
-  res.redirect('/');
-})
-
 const port = process.env.PORT || 3000;
-http.listen(port, function() {
+http.listen(port, () => {
   console.log('Server running on port ' + port);
 });
---
+++ routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/admin', function (req, res) {
+  res.render('admin');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { flash: req.flash() });
+});
+
+router.post('/login', function (req, res, next) {
+  if (req.body.username && req.body.username === 'user' && req.body.password && req.body.password === 'pass') {
+    req.session.authenticated = true;
+    res.redirect('/admin');
+  } else {
+    req.flash('error', 'Incorrect username / password');
+    res.redirect('/login');
+  }
+});
+
+router.get('/logout', function (req, res, next) {
+  delete req.session.authenticated;
+  res.redirect('/');
+});
+
+
+module.exports = router;
