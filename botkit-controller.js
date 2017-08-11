d20bdeb73c6ae270f645e21ce7e87c86ccc9e9ba
Preston Richey <prichey@barkleyus.com>
2017-08-11 17:29:29 -0500

    Add logging

--- botkit-controller.js
+++ botkit-controller.js
@@ -1,5 +1,11 @@
 const TwilioSMSBot = require('botkit-sms');
 const leven = require('fast-levenshtein');
+const fs = require('fs');
+const path = require('path');
+const Promise = require('bluebird');
+const moment = require('moment');
+
+const logPath = './public/logs/test.log';
 
 const rooms = require('./rooms.json');
 const roomList = rooms.map(roomObj => roomObj.name);
@@ -9,6 +15,43 @@ const controller = TwilioSMSBot({
   auth_token: process.env.TWILIO_AUTH_TOKEN,
   twilio_number: process.env.TWILIO_NUMBER
 });
+const bot = controller.spawn({});
+
+function log(str) {
+  let logStr = `[${moment().format('MM/DD/YY HH:mm:ss')}] ${str}`;
+  console.log(logStr);
+
+  logStr += '\n';
+
+  return new Promise((resolve, reject) => {
+    fs.appendFile(logPath, logStr, err => {
+      reject(err);
+    });
+
+    resolve();
+  });
+}
+
+function logMessageReceived(message) {
+  log(`Received message '${message.text}' from ${message.from}`);
+}
+
+function logQuestionAnswered(message, to) {
+  log(`Received answer '${message}' from ${to}`);
+}
+
+function logMessageSent(message, to) {
+  log(`Sending message '${message}' to ${to}`);
+}
+
+function logQuestionAsked(message, to) {
+  log(`Asking question '${message}' to ${to}`);
+}
+
+function replyToMessage(message, text) {
+  logMessageSent(text, message.from);
+  bot.reply(message, text);
+}
 
 function getRoomFromRoomList(searchRoom) {
   if (!!searchRoom && searchRoom.length > 0) {
@@ -61,26 +104,28 @@ function getRoomFromRoomListUsingLevensheinDistance(query) {
 }
 
 module.exports = function(app) {
-  let bot = controller.spawn({});
-
   controller.createWebhookEndpoints(app, bot, function() {
     controller.startTicking();
-    console.log('TwilioSMSBot is online!');
+    log('TwilioSMSBot is online!');
   });
 
   controller.hears(roomList, 'message_received', (bot, message) => {
+    logMessageReceived(message);
     if (!!message.match && message.match.length > 0) {
       const roomObj = getRoomFromRoomList(message.match[0]);
       if (roomObj && roomObj.location) {
-        bot.reply(message, roomObj.location);
+        replyToMessage(message, roomObj.location);
       } else {
-        bot.reply(
+        replyToMessage(
           message,
           `Sorry, I'm not sure where that is. Maybe ask Eddie?`
         );
       }
     } else {
-      bot.reply(message, `Sorry, I'm not sure where that is. Maybe ask Eddie?`);
+      replyToMessage(
+        message,
+        `Sorry, I'm not sure where that is. Maybe ask Eddie?`
+      );
     }
   });
 
@@ -88,7 +133,8 @@ module.exports = function(app) {
     ['hi', 'hello', 'who are you', 'what is this'],
     'message_received',
     (bot, message) => {
-      bot.reply(
+      logMessageReceived(message);
+      replyToMessage(
         message,
         `Hi there! I'm Carmen, a chatbot made by Moonshot to help you find any room in the building. To use me, just ask, "Where's Uranus", for example.`
       );
@@ -99,48 +145,59 @@ module.exports = function(app) {
     ['thanks', 'thank you'],
     'message_received',
     (bot, message) => {
-      bot.reply(message, `You're welcome!`);
+      logMessageReceived(message);
+      replyToMessage(message, `You're welcome!`);
     }
   );
 
   controller.hears('.*', 'message_received', (bot, message) => {
     bot.startConversation(message, (err, convo) => {
+      logMessageReceived(message);
       const query = removeQuestionWordsFromString(message.text);
       const roomObj = getRoomFromRoomListUsingLevensheinDistance(query);
 
       if (roomObj !== null) {
-        convo.ask(
-          `I'm not exactly sure what you meant. Where you asking how to find ${roomObj.name}?`,
-          [
-            {
-              pattern: bot.utterances.yes,
-              callback: (res, convo) => {
-                convo.say(roomObj.location);
-                convo.next();
-              }
-            },
-            {
-              pattern: bot.utterances.no,
-              callback: (res, convo) => {
-                convo.say(
-                  `Unfortunately, I don't understand what you're asking. Maybe try asking a different way, or check your spelling! 🤷‍`
-                );
-                convo.next();
-              }
-            },
-            {
-              default: true,
-              callback: (res, convo) => {
-                convo.say(
-                  `I don't understand. Maybe try asking a different way!`
-                );
-                convo.next();
-              }
+        const question = `I'm not exactly sure what you meant. Where you asking how to find ${roomObj.name}?`;
+        logQuestionAsked(question, message.from);
+        convo.ask(question, [
+          {
+            pattern: bot.utterances.yes,
+            callback: (res, convo) => {
+              logQuestionAnswered(res.text, message.to);
+
+              const messageToSend = roomObj.location;
+              logMessageSent(messageToSend, message.from);
+              convo.say(messageToSend);
+              convo.next();
             }
-          ]
-        );
+          },
+          {
+            pattern: bot.utterances.no,
+            callback: (res, convo) => {
+              logQuestionAnswered(res.text, message.to);
+
+              const messageToSend = `Unfortunately, I don't understand what you're asking. Maybe try asking a different way, or check your spelling! 🤷‍`;
+              logMessageSent(messageToSend, message.from);
+              convo.say(messageToSend);
+              convo.next();
+            }
+          },
+          {
+            default: true,
+            callback: (res, convo) => {
+              logQuestionAnswered(res.text, message.to);
+
+              const messageToSend = `I don't understand. Maybe try asking a different way!`;
+              logMessageSent(messageToSend, message.from);
+              convo.say(messageToSend);
+              convo.next();
+            }
+          }
+        ]);
       } else {
-        convo.say(`I'm not sure what you mean. Try asking a different way!`);
+        const messageToSend = `I'm not sure what you mean. Try asking a different way!`;
+        logMessageSent(messageToSend, message.from);
+        convo.say(messageToSend);
         convo.next();
       }
     });
