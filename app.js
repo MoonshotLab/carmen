c080f8f42156ee83100cf072efec40e461f7a051
Preston Richey <prichey@barkleyus.com>
2017-08-17 11:59:49 -0500

    Remove checkAuth function; Add mongoose dependency

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ const path = require('path');
 
 const routes = require('./routes');
 
-function checkAuth(req, res, next) {
-  console.log('checkAuth ' + req.url);
-  console.log('checking auth');
-
-  // don't serve /secure to those not logged in
-  // you should add to this list, for each and every secure url
-  if (req.url === '/admin' && (!req.session || !req.session.authenticated)) {
-    res.redirect('/login');
-    return;
-  }
-
-  next();
-}
-
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,7 +25,6 @@ app.use(
 );
 app.use(flash());
 
-// app.use(checkAuth);
 app.use('/', routes);
 
 require('./botkit-controller')(app);
