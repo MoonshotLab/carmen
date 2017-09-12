abdb9459b5ff538880df328e4aace74291de5e9a
Preston Richey <prichey@barkleyus.com>
2017-09-12 17:13:23 -0500

    Fill out stats template

--- routes/stats.js
+++ routes/stats.js
@@ -1,8 +1,16 @@
+const moment = require('moment');
 const express = require('express');
 const router = express.Router();
 
+const util = require('./../lib/util');
+const db = require('./../lib/db');
+
 router.get('/', (req, res) => {
-  res.render('stats');
+  const statsObj = db.getFormattedStatsObj();
+
+  res.render('stats', {
+    stats: statsObj
+  });
 });
 
 module.exports = router;
