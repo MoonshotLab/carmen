c13bc36efc752c862747b6ca7fe197743d488c3d
Preston Richey <prichey@barkleyus.com>
2017-09-14 15:48:13 -0500

    Reformat stats and add feedback

--- routes/stats.js
+++ routes/stats.js
@@ -9,7 +9,10 @@ router.get('/', (req, res) => {
   const statsObj = db.getFormattedStatsObj();
 
   res.render('stats', {
-    stats: statsObj
+    stats: statsObj,
+    formatMoment: date => moment(date).format('l'),
+    formatPhone: phone =>
+      phone.replace('+1', '').replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3')
   });
 });
 
